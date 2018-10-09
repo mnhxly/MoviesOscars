d5334b7405ddd567dc6d7e439f3e8044f2289a02
malte <malte.hoenicke@gmail.com>
2018-10-09 12:22:19 +0200

    save

---
+++ MoviesOscars/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesOscars
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ MoviesOscars/MovieOscar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesOscars
+{
+    public class MovieOscar
+    {
+        public Movie Movie { get; set; }
+        public Oscar Oscar { get; set; }
+        public int Year { get; set; }
+    }
+}
