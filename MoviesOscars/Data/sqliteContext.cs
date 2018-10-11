c0693d92db8e79f3451eb6b005547e6871534052
Your Name <you@example.com>
2018-10-11 14:42:24 +0200

    save

---
+++ MoviesOscars/Data/sqliteContext.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using BerlinReligionClassData.Models;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace MoviesOscars.Data
+//{
+//    public class ReligionDatabaseContext : DbContext
+//    {
+//        /// <summary>
+//        /// Gets or sets the subventions.
+//        /// </summary>
+//        public DbSet<Movie> Subventions { get; set; }
+//        public DbSet<Oscar> Participants { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+//            optionsBuilder.UseSqlite("Filename=./MODataBase.db");
+//        }
+
+//    }
+//}
